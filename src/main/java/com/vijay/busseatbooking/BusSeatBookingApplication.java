6aab5e69828fedda34cdcbe67415d698a11b9c3d
Vijay V A <105069143+Vijay-V-A@users.noreply.github.com>
2025-02-19 19:13:34 +0530

    First commit

---
+++ src/main/java/com/vijay/busseatbooking/BusSeatBookingApplication.java
@@ -0,0 +1,13 @@
+package com.vijay.busseatbooking;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BusSeatBookingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BusSeatBookingApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/vijay/busseatbooking/BusSeatBookingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vijay.busseatbooking;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BusSeatBookingApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
